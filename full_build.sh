40115fbd30658577f75dd7e26656790c80215754
William Deegan <bill@baddogconsulting.com>
2018-07-22 11:38:37 -0700

    updates from other python build script repo

--- full_build.sh
+++ full_build.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 base_dir=${HOME}/tools
-python_version=3.6.1
+python_version=2.7.13
+#python_version=3.6.2
 python_dir=python-${python_version}
 kits_dir=${base_dir}/kits
 install_dir=${base_dir}/${python_dir}
@@ -12,9 +13,17 @@ install_dir=${base_dir}/${python_dir}
 mkdir -p ${base_dir}/${python_dir}/lib
 mkdir -p ${kits_dir}
 
+# Ubuntu
+#sudo apt-get install build-essential
+#sudo apt-get build-dep python2.7
 
-sudo apt-get install build-essential
-sudo apt-get build-dep python2.7
+# Centos 7
+#sudo yum install -y rpm-build
+#sudo yum install -y redhat-rpm-config
+#sudo yum install -y yum-utils
+#sudo yum groupinstall -y "Development Tools" --setopt=group_package_types=mandatory,default,optional
+##sudo yum-builddep python
+
 
 pushd ${kits_dir}
 if [ ! -f Python-${python_version}.tgz ]; then
@@ -43,7 +52,7 @@ make install
 popd
 
 pushd libxml2-2.9.2
-LDFLAGS="-Wl,-rpath=$HOME/tools/python-${python_version}/lib" ./configure --prefix=${install_dir} --enable-shared
+LDFLAGS="-Wl,-rpath=$HOME/tools/python-${python_version}/lib" ./configure --prefix=${install_dir} --enable-shared --with-python=$HOME/tools/python-${python_version}
 make
 make install
 popd
@@ -54,4 +63,11 @@ make
 make install
 popd
 
+wget https://bootstrap.pypa.io/get-pip.py
 
+# If Py3, use python3 as binary name
+if [[ "$python_version" == 3.* ]]; then
+    python3 get-pip.py
+else
+    python get-pip.py
+fi
